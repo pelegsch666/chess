cc684adce7ff0333e683f184ba6509d2ddc304c3
pelegsch666 <peleg.mazuz@gmail.com>
2023-07-30 17:35:44 +0300

    last

--- src/referee/referee.ts
+++ src/referee/referee.ts
@@ -1,5 +1,21 @@
+import { PieceType, TeamType } from "../components/Chessboard";
+
 export default class Referee { 
-    isValidMove(px:number,py:number ,x: number,y: number, type: ) {
-        return true;
+    isValidMove(px:number,py:number ,x: number,y: number, type: PieceType, team:TeamType) {
+    console.log(`prev: ${px},${py} next: ${x},${y} type: ${type} tean: ${team}`)
+       
+    
+    
+    if(type === PieceType.PAWN){
+     if(team === TeamType.OURTEAM){
+        if(py ===1){
+            if(px === x && (y - py === 1 || y -py ===2)){
+               console.log("valid move")
+                return true;
+            }
+        }
+     }
+    }
+    return false;
     }
 }
