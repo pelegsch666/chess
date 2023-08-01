81745203120617af1838ee4bbcf63d377654dc52
pelegsch666 <peleg.mazuz@gmail.com>
2023-08-01 20:05:23 +0300

    improved

--- src/referee/referee.ts
+++ src/referee/referee.ts
@@ -22,37 +22,35 @@ export default class Referee {
     console.log(
       `prev: ${px},${py} next: ${x},${y} type: ${type} tean: ${team}`
     );
-
     if (type === PieceType.PAWN) {
-      if (team === TeamType.OURTEAM) {
-        if (py === 1) {
-          if (px === x && (y - py === 1 || y - py === 2)) {
-            if (!this.tileIsOccupied(x, y, boardState)) {
-              return true;
-            }
-            return true;
-          }
-        } else {
-          if (px === x && y - py === 1) {
-            if (!this.tileIsOccupied(x, y, boardState)) {
-              return true;
-            }
+      const specialRow = team === TeamType.OURTEAM ? 1 : 6;
+      const pawnDirection = team === TeamType.OURTEAM ? 1 : -1;
+
+      if (py === specialRow) {
+        if (px === x && y - py === 2 * pawnDirection) {
+          if (
+            !this.tileIsOccupied(x, y, boardState) &&
+            !this.tileIsOccupied(x, y - pawnDirection, boardState)
+          ) {
             return true;
           }
         }
+      } else if (px === x && y - py === 2 * pawnDirection) {
+        if (
+          !this.tileIsOccupied(x, y, boardState) &&
+          !this.tileIsOccupied(x, y - pawnDirection, boardState)
+        ) {
+          return true;
+        }
       } else {
-        if (py === 6) {
-          if ((px === x && y - py === -1) || y - py === -2) {
-            console.log('valid move');
-            return true;
-          }
-        } else {
-          if (px === x && y - py === -1) {
+        if (px === x && y - py === pawnDirection) {
+          if (!this.tileIsOccupied(x, y, boardState)) {
             return true;
           }
         }
       }
     }
+
     return false;
   }
 }
