7f95e0beb9ca961f0048ad0feb16f7565bfbded6
pelegsch666 <peleg.mazuz@gmail.com>
2023-08-02 19:17:20 +0300

    prog

--- src/referee/referee.ts
+++ src/referee/referee.ts
@@ -9,7 +9,19 @@ export default class Referee {
       return false;
     }
   }
-
+  TileIsOccupiedByOpponent(
+    x: number,
+    y: number,
+    team: TeamType,
+    boardState: Piece[]
+  ): boolean {
+    const piece = boardState.find(p => p.x === x && p.y === y);
+    if (piece && piece.team !== team) {
+      return true;
+    } else {
+      return false;
+    }
+  }
   isValidMove(
     px: number,
     py: number,
@@ -25,29 +37,29 @@ export default class Referee {
     if (type === PieceType.PAWN) {
       const specialRow = team === TeamType.OURTEAM ? 1 : 6;
       const pawnDirection = team === TeamType.OURTEAM ? 1 : -1;
-
-      if (py === specialRow) {
-        if (px === x && y - py === 2 * pawnDirection) {
-          if (
-            !this.tileIsOccupied(x, y, boardState) &&
-            !this.tileIsOccupied(x, y - pawnDirection, boardState)
-          ) {
-            return true;
-          }
-        }
-      } else if (px === x && y - py === 2 * pawnDirection) {
+      // movement logic
+      if (px === x && py === specialRow && y - py === 2 * pawnDirection) {
         if (
           !this.tileIsOccupied(x, y, boardState) &&
           !this.tileIsOccupied(x, y - pawnDirection, boardState)
         ) {
           return true;
         }
-      } else {
-        if (px === x && y - py === pawnDirection) {
-          if (!this.tileIsOccupied(x, y, boardState)) {
-            return true;
-          }
+      } else if (px === x && y - py === pawnDirection) {
+        if (!this.tileIsOccupied(x, y, boardState)) {
+          return true;
         }
+      }
+      // Attack Logic
+      else if (x - px === -1 && y - py === pawnDirection) {
+        //Attack in upper or bottom left
+        console.log('Attack in upper or bottom left');
+        if(!this.TileIsOccupiedByOpponent(x, y, team, boardState)){
+         console.log('We can strike the enemy')
+        }
+      } else if (x - px === 1 && y - py === pawnDirection) {
+        //Attack in upper or bottom right
+        console.log('Attack in upper or bottom right');
       }
     }
 
